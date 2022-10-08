669126b1529d682245a892f8f4ed26fe073064a2
sehaj chawla <sehajchawla29@gmail.com>
2022-10-08 21:27:53 +0530

    adding home page of frontend

--- clientside/public/scripts/app.js
+++ clientside/public/scripts/app.js
@@ -58,3 +58,33 @@ var fetchData=()=>{
         }
     });
 }
+var fetchData=()=>{
+    // alert("button is clicked.....");
+     let url="http://localhost:8000/api/products";
+     //use Ajax mechanism to fetch data from  rest api
+     //it is inbuilt function of jQuery Library
+     $.ajax({
+         dataType: "json",
+         url: url,
+         type:"GET",
+         success: (data)=>{
+             console.log(data);
+             let strData=JSON.stringify(data)
+             //alert(strData);
+            /* let para1=document.getElementById("para");
+             para1.innerHTML=strData;
+             */
+             //DOM tree Manipulation Code at Client Side
+             let productList=document.getElementById("productList");
+             for(var i=0;i<data.length;i++){
+                 const node = document.createElement("li");
+                 const textnode = document.createTextNode(data[i].title);
+                 node.appendChild(textnode);
+                 productList.appendChild(node);
+             }  
+         }
+       });
+     //on receive data dynamically append products names to existing HTML page
+     //DOM Manipulation
+     console.log("Button is Clicked......");
+ }
